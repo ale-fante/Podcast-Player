4f2e4493a31d662c7dc5a0e36cb6b27cdfdf6b34
Alejandra Ledesma <Alejandra.Ledesma@nick.com>
2019-05-14 15:04:31 -0700

    Split view controller

---
+++ Podcast Player/Parser.swift
@@ -0,0 +1,16 @@
+//
+//  Parser.swift
+//  Podcast Player
+//
+//  Created by Ledesma, Alejandra on 5/14/19.
+//  Copyright © 2019 Ledesma, Alejandra. All rights reserved.
+//
+
+import Foundation
+
+
+class Parser {
+    func getPodcastMataData(data:Data) {
+        print("WOHOOO")
+    }
+}
---
+++ Podcast Player/PodcastViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PodcastViewController.swift
+//  Podcast Player
+//
+//  Created by Ledesma, Alejandra on 5/14/19.
+//  Copyright © 2019 Ledesma, Alejandra. All rights reserved.
+//
+
+import Cocoa
+
+class PodcastViewController: NSViewController {
+    
+    @IBOutlet weak var podcastURLTextField: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        podcastURLTextField.stringValue = "http://www.espn.com/espnradio/podcast.feeds/itunes/podCast?id=2406595"
+    }
+    
+    @IBAction func addPodcastClicked(_ sender: Any) {
+        
+        if let url = URL(string: podcastURLTextField.stringValue){
+            URLSession.shared.dataTask(with: url) { (data:Data?, response:URLResponse?, error:Error?) in
+                if error != nil {
+                    print("BIG FAIL")
+                    
+                } else{
+                    if data != nil{
+                        let parser = Parser()
+                        parser.getPodcastMataData(data: data!)
+                    }
+                    
+                }
+                }.resume()
+        }
+    }
+}
