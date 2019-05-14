3500ef47e52b713cbd537b65d58ac049f94e9ee8
Alejandra Ledesma <Alejandra.Ledesma@nick.com>
2019-05-14 09:22:10 -0700

    Initial Commit

---
+++ Podcast Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Podcast Player
+//
+//  Created by Ledesma, Alejandra on 5/14/19.
+//  Copyright © 2019 Ledesma, Alejandra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
